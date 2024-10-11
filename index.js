fd695966123933641e4c37a9748ebb8b69d5afe8
Ryan Gilson <ryan.gilson@pointivo.com>
2024-10-11 16:59:13 -0400

    Support multiple source maps for a given stack trace

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-'use strict';
-const meow = require('meow');
-const stackTraceParser = require('stacktrace-parser');
-const fs = require('fs-extra');
-const clipboardy = require('clipboardy');
-const { SourceMapConsumer } = require('source-map');
+"use strict";
+const meow = require("meow");
+const stackTraceParser = require("stacktrace-parser");
+const fs = require("fs-extra");
+const clipboardy = require("clipboardy");
+const { SourceMapConsumer } = require("source-map");
 
-
-const cli = meow(`
+const cli = meow(
+  `
   Usage
     $ stacktracify <map-path>
 
@@ -15,32 +15,88 @@ const cli = meow(`
     --file, -f  (default is read from clipboard)
 
   Examples
-    $ stacktracify /path/to/js.map --file /path/to/my-stacktrace.txt
-`, {
-  flags: {
-    file: {
-      type: 'string',
-      alias: 'f',
+    $ stacktracify --map /path/to/js.map --file /path/to/my-stacktrace.txt
+`,
+  {
+    flags: {
+      file: {
+        type: "string",
+        alias: "f",
+      },
+      map: {
+        type: "string",
+        alias: "m",
+        isMultiple: true,
+      },
     },
-  },
-});
+  }
+);
 
-
-const { file } = cli.flags;
+const { map: mapPath, file } = cli.flags;
 
 (async () => {
   try {
-    const mapPath = cli.input[0];
+    console.log(mapPath, file);
+    const consumers = [];
+    let preferredConsumerOrder = false;
     if (!mapPath) cli.showHelp();
-    const mapContent = JSON.parse(await fs.readFile(mapPath, 'utf-8'));
-    // WTF? promise?
-    const smc = await new SourceMapConsumer(mapContent);
+
+    try {
+      if (Array.isArray(mapPath)) {
+        await Promise.all(
+          mapPath.map(async (m) => {
+            const mapContent = JSON.parse(await fs.readFile(m, "utf-8"));
+            const smc = await new SourceMapConsumer(mapContent);
+
+            consumers.push(smc);
+          })
+        );
+      } else {
+        const mapContent = JSON.parse(await fs.readFile(mapPath, "utf-8"));
+        const smc = await new SourceMapConsumer(mapContent);
+
+        consumers.push(smc);
+      }
+    } catch (err) {
+      console.log("An error occurred loading source map consumers!", err);
+    }
+    // support multiple source map declarations
+
+    if (!consumers?.length) {
+      throw new Error("Unable to resolve source map consumer!");
+    }
 
     let str;
     if (file !== undefined) {
-      str = await fs.readFile(file, 'utf-8');
+      str = await fs.readFile(file, "utf-8");
     } else {
       str = await clipboardy.read();
+    }
+
+    function findPositionInAllSourceMaps(lookup) {
+      if (!consumers.length) return;
+
+      let index = 0;
+      for (const consumer of consumers) {
+        const lookupValue = consumer.originalPositionFor(lookup);
+
+        if (lookupValue?.line) {
+          if (!preferredConsumerOrder) {
+            if (index === 0) {
+              preferredConsumerOrder = true;
+            } else {
+              const swap = consumer[0];
+              consumer[0] = lookupValue;
+              consumer[index] = swap;
+              preferredConsumerOrder = true;
+            }
+          }
+
+          return lookupValue;
+        }
+
+        index++;
+      }
     }
 
     let [header, ...lines] = str.trim().split(/\r?\n/);
@@ -54,27 +110,29 @@ const { file } = cli.flags;
       }
 
       return line;
-    })
+    });
 
-    const stack = stackTraceParser.parse(lines.join('\n'));
-    if (stack.length === 0) throw new Error('No stack found');
+    const stack = stackTraceParser.parse(lines.join("\n"));
+    if (stack.length === 0) throw new Error("No stack found");
 
     if (header) console.log(header);
 
     stack.forEach(({ methodName, lineNumber, column }) => {
       try {
         if (lineNumber == null || lineNumber < 1) {
-          console.log(`    at ${methodName || '[unknown]'}`);
+          console.log(`    at ${methodName || "[unknown]"}`);
         } else {
-          const pos = smc.originalPositionFor({ line: lineNumber, column });
+          const pos = findPositionInAllSourceMaps({ line: lineNumber, column });
           if (pos && pos.line != null) {
-            console.log(`    at ${pos.name || methodName || '[unknown]'} (${pos.source}:${pos.line}:${pos.column})`);
+            console.log(
+              `    at ${pos.name || methodName || "[unknown]"} (${pos.source}:${
+                pos.line
+              }:${pos.column})`
+            );
           }
-    
-          // console.log('src', smc.sourceContentFor(pos.source));
         }
       } catch (err) {
-        console.log(`    at FAILED_TO_PARSE_LINE`);
+        console.log(err, `    at FAILED_TO_PARSE_LINE`);
       }
     });
   } catch (err) {
